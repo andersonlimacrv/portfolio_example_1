87a5252f80325aa11868cbd7fed65a2231e7687a
Anderson_Carvalho <andersonlimacrv@gmail.com>
2023-08-25 19:39:24 -0300

    feat: change logo.

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = () => {
         <div className='bg-primary/60 h-full'>
             {/* text*/}
             <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-                <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+                <div className='text-center flex flex-col justify-center xl:pt-30 xl:text-left h-full container mx-auto pt-5 xxl:pt-0'>
                     {/* title */}
                     <motion.h1
                         variants={fadeIn('down', '0.2')}
@@ -36,7 +36,7 @@ const Home = () => {
                         initial='hidden'
                         animate='show'
                         exit='hidden'
-                        className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-[white]'
+                        className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
                     >
                         I’m a frontend developer with a particular interest in making things simple and automating daily tasks.
                         I try to keep up with security and best practices, and am always looking for new things to learn.
