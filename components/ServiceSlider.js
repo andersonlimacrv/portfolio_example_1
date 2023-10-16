87c6b4b0dba03c4dd62d8009371ad9aefc4d0c7c
Anderson_Carvalho <andersonlimacrv@gmail.com>
2023-10-16 10:07:12 -0300

    feat: finish slider, colors ok.

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -17,7 +17,7 @@ import {
 } from "react-icons/rx";
 
 // import required modules
-import { Pagination, FreeMode } from "swiper";
+import { Autoplay, Pagination, FreeMode } from "swiper";
 
 
 // service data
@@ -53,28 +53,39 @@ const ServiceSlider = () => {
     return <Swiper breakpoints={{
         320: {
             slidesPerView: 1,
-            spaceBetween: 15,
+            spaceBetween: 30,
+
         },
-        640: {
+        768: {
             slidesPerView: 3,
             spaceBetween: 15,
         },
     }}
         freeMode={true}
+        autoplay={{
+            delay: 4500,
+            disableOnInteraction: true,
+        }}
         pagination={{
             clickable: true,
         }}
-        modules={[Pagination, FreeMode]}
-        className="h-[240px] sm:h-[340px">
+        modules={[Pagination, FreeMode, Autoplay]}
+        className="h-[240px] sm:h-[340px]">
         {
             serviceData.map((item, index) => {
                 return <SwiperSlide key={index}>
-                    <div>
+                    <div className='bg-black/10 backdrop-blur-sm h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(39,42,88,0.3)] transition-all duration-200'>
                         { /* icon */}
-                        <div className='text-accent text-3xl mb-4'>{item.icon}</div>
+                        <div className='text-accent text-4xl mb-4'>{item.icon}</div>
                         {/* title & description */}
-                        <div className='h3'>{item.title}</div>
-                        <p>{item.description}</p>
+                        <div className='mb-8'>
+                            <div className='mb-4 text-lg'>{item.title}</div>
+                            <p className='max-w-[400px] leading-normal'>{item.description}</p>
+                        </div>
+                        {/* Arrow */}
+                        <div className='text-accent text-3xl'>
+                            <RxArrowTopRight />
+                        </div>
                     </div>
                 </SwiperSlide>
             })
--- pages/services/index.js
+++ pages/services/index.js
@@ -17,12 +17,14 @@ const Services = () => {
                     {/* text */}
                     <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
                         <h2 className='h2 xl:mt-8'>My services<span className='text-accent'>.</span></h2>
-                        <p className='mb-4 max-w-[400px] mx-auto xl:mx-0'> lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'> lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
                     </div>
-                    {/* slider */}
-                    <ServiceSlider />
+                    <div className='w-full xl:max-w-[65%]'>
+                        {/* slider */}
+                        <ServiceSlider />
+                    </div>
                 </div>
             </div>
             <Bulb />
